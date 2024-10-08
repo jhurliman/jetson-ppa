d33630f69c99dbc21bd052cfc04e1621b70fb01f
John Hurliman <jhurliman@jhurliman.org>
2024-10-08 15:28:19 -0700

    Build ffmpeg 4.4.2 for jetson-r32 (#9)

---
+++ docker/container-build-ffmpeg-jetson-r32.sh
@@ -0,0 +1,31 @@
+# Build and package ffmpeg for Jetson
+
+# Increment this whenever the build script or Dockerfile changes
+CPACK_PACKAGE_RELEASE=1
+
+# This folder is bind-mounted from the host machine
+cd /build
+
+cmake \
+  -G Ninja \
+  -D CMAKE_BUILD_TYPE=Release \
+  -D CMAKE_C_FLAGS="-march=armv8-a+crc" \
+  -D CMAKE_INSTALL_PREFIX=/usr \
+  -D CPACK_BINARY_DEB=ON \
+  -D CPACK_DEBIAN_PACKAGE_REPLACES="libavcodec-dev, libavdevice-dev, libavformat-dev, libavfilter-dev, libavutil-dev, libswresample-dev, libswscale-dev, libavcodec57, libavdevice57, libavformat57, libavfilter6, libavutil55, libswresample, libswscale" \
+  -D CPACK_DEBIAN_PACKAGE_PROVIDES="libavcodec-dev, libavdevice-dev, libavformat-dev, libavfilter-dev, libavutil-dev, libswresample-dev, libswscale-dev, libavcodec58, libavdevice58, libavformat58, libavfilter7, libavutil56, libswresample, libswscale" \
+  -D CPACK_DEBIAN_PACKAGE_MAINTAINER="John Hurliman" \
+  -D CPACK_DEBIAN_PACKAGE_NAME="ffmpeg" \
+  -D CPACK_GENERATOR=DEB \
+  -D CPACK_SOURCE_GENERATOR=DEB \
+  -D CPACK_PACKAGE_CONTACT="John Hurliman <jhurliman@jhurliman.org>" \
+  -D CPACK_PACKAGE_NAME="ffmpeg" \
+  -D CPACK_PACKAGE_VERSION="4.4.2" \
+  -D CPACK_PACKAGE_RELEASE="${CPACK_PACKAGE_RELEASE}" \
+  -D CPACK_PACKAGE_DESCRIPTION="FFmpeg is the leading multimedia framework, able to decode, encode, transcode, mux, demux, stream, filter and play pretty much anything that humans and machines have created. It supports the most obscure ancient formats up to the cutting edge." \
+  -D CPACK_DEBIAN_PACKAGE_SHLIBDEPS=ON \
+  -D CPACK_DEBIAN_FILE_NAME="DEB-DEFAULT" \
+  /usr/src
+
+# Build ffmpeg and create a deb package
+ninja package
---
+++ scripts/publish-ffmpeg.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -e
+
+PLATFORMS=("jetson-r32" "x64")
+S3_BUCKET=repo.download.mvi.llc
+VERSION=4.4.2
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PLATFORM=$1
+
+# Validate arguments
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 <platform (${PLATFORMS[@]})>"
+  exit 1
+fi
+if [[ ! " ${PLATFORMS[@]} " =~ " ${PLATFORM} " ]]; then
+  echo "Invalid platform: ${PLATFORM}"
+  echo "Valid platforms: ${PLATFORMS[@]}"
+  exit 1
+fi
+
+# Ensure the .deb exists
+if [ "${PLATFORM}" == "x64" ]; then
+  DEB_FILENAME="ffmpeg_${VERSION}_amd64.deb"
+else
+  DEB_FILENAME="ffmpeg_${VERSION}_arm64.deb"
+fi
+BUILD_DIR="${SCRIPT_DIR}/../build/ffmpeg-${PLATFORM}"
+if [ ! -f ${BUILD_DIR}/${DEB_FILENAME} ]; then
+  echo "${BUILD_DIR}/${DEB_FILENAME} not found. Run ./scripts/build.sh ${PLATFORM} ffmpeg first."
+  exit 1
+fi
+
+# Ensure GPG_PUBLIC_KEY and GPG_PRIVATE_KEY are set
+if [ -z "${GPG_PUBLIC_KEY}" ] || [ -z "${GPG_PRIVATE_KEY}" ]; then
+  echo "GPG_PUBLIC_KEY and GPG_PRIVATE_KEY must be set."
+  exit 1
+fi
+
+cd ${SCRIPT_DIR}/../docker
+
+# Build deb-s3 CLI utility tool image
+docker build -t deb-s3 -f Dockerfile.deb-s3 .
+
+if [ "${PLATFORM}" == "jetson-r32" ]; then
+  DEB_S3_CMD="deb-s3 upload --bucket "${S3_BUCKET}" --arch arm64 --prefix jetson/common --codename=r32.7 --sign ${GPG_PUBLIC_KEY} /build/${DEB_FILENAME}"
+elif [ "${PLATFORM}" == "x64" ]; then
+  DEB_S3_CMD="deb-s3 upload --bucket "${S3_BUCKET}" --arch amd64 --prefix ubuntu/x64 --codename=jammy --sign ${GPG_PUBLIC_KEY} /build/${DEB_FILENAME}"
+fi
+
+# Upload the ffmpeg deb package to S3
+docker run --rm \
+  -v ~/.aws:/root/.aws \
+  -v ${BUILD_DIR}:/build \
+  -e GPG_PRIVATE_KEY="${GPG_PRIVATE_KEY}" \
+  ${DEB_S3_CMD}
